b348b22ff55788552b5172f157af7b623faaa802
Dominik Chrastek <dominik.chrastek@dactylgroup.cz>
2014-11-01 17:23:06 +0100

    search box

---
+++ lib/input-search/input-search.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2014, Nouvelle Communauté. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:polymer/polymer.dart';
+
+/// A Polymer <list-item-element> element.
+@CustomTag('input-search')
+class InputSearch extends PolymerElement {
+
+  InputSearch.created() : super.created();
+
+}
