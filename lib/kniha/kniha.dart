d0bfe4561a14ed2f4eb6039219b7e666300a4f66
HonzaBittner <the@honzabittner.cz>
2014-11-01 12:41:45 +0100

    Add element kniha

---
+++ lib/kniha/kniha.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2014, Nouvelle Communauté. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:polymer/polymer.dart';
+
+/// A Polymer <main-app> element.
+@CustomTag('kniha')
+class Kniha extends PolymerElement {
+
+  Kniha.created() : super.created();
+
+}
