0a5c9a4700f04b84b64a3f4e055cf8fc932771c2
Mike <molsavsky1@gmail.com>
2014-11-01 14:09:51 +0100

    Binding Profile

--- lib/profile-element/profile-element.dart
+++ lib/profile-element/profile-element.dart
@@ -2,11 +2,32 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:polymer/polymer.dart';
+import 'dart:convert' show JSON;
+import 'dart:html';
 
 /// A Polymer <profile-element> element.
 @CustomTag('profile-element')
 class ProfileElement extends PolymerElement {
 
-  ProfileElement.created() : super.created();
+  @observable
+  Object profile;
+  
+  ProfileElement.created()
+    : super.created() {
+    
+    this.makeRequest();
+  }
 
+  void makeRequest() {
+    var path = 'data/profile.json';
+       
+    HttpRequest.getString(path)
+    .then(loadJSON)
+    .catchError((e) => window.alert(e.toString()));
+  }
+  
+  void loadJSON(String content) {
+    this.profile = JSON.decode(content);
+    print(this.profile);
+  }
 }
