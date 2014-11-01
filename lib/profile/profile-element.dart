3ab33c297ce2df6aed4c5542e1a85579a484cea4
Mike <molsavsky1@gmail.com>
2014-11-01 12:46:14 +0100

    Oprava Hobbiho

---
+++ lib/profile/profile-element.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2014, Nouvelle Communauté. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:polymer/polymer.dart';
+
+/// A Polymer <profile-element> element.
+@CustomTag('profile-element')
+class ProfileElement extends PolymerElement {
+
+  ProfileElement.created() : super.created();
+
+}
