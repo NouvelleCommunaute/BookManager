71c3d5c81e2d86bd2d155ebc458561321d3ad836
Tomas Nyvlt <nnyvlt@gmail.com>
2014-11-01 12:22:39 +0100

    rename, drop-down

---
+++ lib/drop-down/drop-down.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2014, Nouvelle Communauté. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:polymer/polymer.dart';
+
+/// A Polymer <drop-down> element.
+@CustomTag('drow-down')
+class MainApp extends PolymerElement {
+
+  MainApp.created() : super.created();
+
+}
